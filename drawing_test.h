b3a69e1042bd50d869ebb849e7ea0925b4830439
danielmp <danny.mp@live.com.pt>
2014-05-14 17:38:40 +0100

    failed in doing test of drawing_units.c

--- drawing_test.h
+++ drawing_test.h
@@ -1,8 +1,34 @@
 #ifndef DRAWING_TEST_H
 #define DRAWING_TEST_H
 
-#include "C:\GitHub\ScorchedEarth\drawing.h"
+#include "C:\GitHub\ScorchedEarth\unit.c"
+#include "C:\GitHub\ScorchedEarth\drawing_units.c"
 
 
+TEST (check_one_player_one_enemy) {
+
+    int map_layout_test [100][80] = {{0}};
+
+    test_drawing_units(map_layout_test);
+
+    int counter_PLAYER= 0;
+    int counter_ENEMY = 0;
+    for (int x = 0; x < 100; x++)
+    {
+        for (int y = 0; y < 80; y++)
+        {
+            if (map_layout_test[x][y] == 2)
+            {
+                counter_PLAYER ++;
+            }
+            else if (map_layout_test[x][y] == 3)
+            {
+                counter_ENEMY ++;
+            }
+        }
+    }
+    CHECK ( 1 == counter_PLAYER );
+    CHECK ( 1 == counter_ENEMY );
+}
 
 #endif // DRAWING_TEST_H
--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <UnitTest++.h>
 //#include "shot_test.h"
 #include "drawing_test.h"
-#include "unit_test.h"
+//#include "unit_test.h"
+
 int main()
 {
     return UnitTest::RunAllTests();
