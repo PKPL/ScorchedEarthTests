e6b22a8f100bc3a34f61963f3201e05fe2b192d3
danielmp <danny.mp@live.com.pt>
2014-05-14 15:32:36 +0100

    refacting the name of the header drawing_test.h

--- drawingTest.h
+++
@@ -1,7 +0,0 @@
-#ifndef DRAWINGTEST_H
-#define DRAWINGTEST_H
-
-#include "C:\GitHub\ScorchedEarth\drawing.c"
-
-
-#endif // DRAWINGTEST_H
---
+++ drawing_test.h
@@ -0,0 +1,8 @@
+#ifndef DRAWING_TEST_H
+#define DRAWING_TEST_H
+
+#include "C:\GitHub\ScorchedEarth\drawing.c"
+
+
+
+#endif // DRAWING_TEST_H
